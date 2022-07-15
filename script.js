8bde50789989f443535be5fe68039356a4910fe6
pugazh2064 <109068877+pugazh2064@users.noreply.github.com>
2022-07-15 10:53:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function setCookie(){
-	console.log("Hello");
+function getCookie()
+{
+    let x = document.cookie;
+    alert(x);
+}
+
+function setCookie()
+{
+    document.cookie ="username=ABCD;expires=Thu, 14 July 2022 12:00:00 UTC; path=/"
+	console.log("Done");
 }
